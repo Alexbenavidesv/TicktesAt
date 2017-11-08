5a7789092f319eb1e88b6d912fe78c647ecc1997
Carlos cabarcas <carlos.cape.8@gmail.com>
2017-11-08 14:23:14 -0500

    listado de tickets por rol

--- resources/views/listarTickes.blade.php
+++ resources/views/listarTickes.blade.php
@@ -8,6 +8,7 @@
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main block-center">
 	<h1 class="text-center">Listado de Tickets</h1><br>
 	<table class="table table-striped" align="center" style="width: 80%">
+		@if(count($tickets)>0)
 		<thead>
 			<th>Numero</th>
 			<th>Descripcion</th>
@@ -25,13 +26,14 @@
 			@else
 			<td>{{$t->prioridad}}</td>
 			@endif
-			@if($t->id_consutor==null)
-			<td>No asignado</td>
-			@else
-			<td>$nomconsultor</td>
-			@endif
+			<td>{{$t->consultor}}</td>
 		</tbody>
 		@endforeach
+		@else
+		<div class="bg-danger text-center" style="padding-top: 50px; padding-bottom: 50px"><h4>No hay tickets para mostrar</h4></div>
+		@endif
+		
+		
 	</table>
 </div>
 @endsection
