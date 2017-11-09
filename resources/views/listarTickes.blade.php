0565dffc0a8b235a215f60b4a7aba178185f35ae
Carlos cabarcas <carlos.cape.8@gmail.com>
2017-11-09 11:50:36 -0500

    zoom corregido

--- resources/views/listarTickes.blade.php
+++ resources/views/listarTickes.blade.php
@@ -43,7 +43,7 @@
 			@endif
 			<td>{{$t->consultor}}</td>
 			<td>{{$t->tipo}}</td>
-			<td><a type="button" href="/respuesta/{{$t->id}}" class="btn btn-primary btn-sm" style="width: 30px" id="respuesta"><i class="fa fa-reply-all" aria-hidden="true"></i></a></td>
+			<td><a type="button" href="/respuesta/{{$t->id}}" class="btn btn-primary btn-sm" style="width: 30px" id="respuesta"><i class="fa fa-eye" aria-hidden="true"></i></a></td>
 			@if($t->prioridad=='')
 			<td><a type="button" href="" class="btn btn-success btn-sm" data-toggle="modal" data-id="{{$t->id}}" data-target="#asignar{{$t->id}}" style="width: 30px" id="respuesta"><i class="fa fa-pencil" aria-hidden="true"></i></a></td>
 				@endif
