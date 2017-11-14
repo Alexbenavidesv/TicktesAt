004ef49d95de00a3ecb4b701f74459d061707887
Alex Benavides <alexbenavidesv@gmail.com>
2017-11-14 16:45:12 -0500

    Lista de usuarios

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -75,12 +75,13 @@
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main block-center">
 	<h1 class="text-center">Listado de usuarios</h1><br>
 	<button type="submit" class="btn btn-primary" style="width: 150px; margin-left: 110px;" data-toggle="modal" data-target="#usuarios"><i class="fa fa-plus-square" aria-hidden="true"></i> Crear usuario</button>
-	<table class="table table-striped" align="center" style="width: 80%">
+	<table class="table table-striped" align="center" style="width: 90%">
 		<thead>
 			<th>Nombre</th>
 			<th>Cedula</th>
 			<th>Empresa</th>
 			<th>Correo</th>
+			<th>Teléfono</th>
 			<th>Rol</th>
 			<th>Controles</th>
 		</thead>
@@ -90,6 +91,7 @@
 			<td>{{$us->email}}</td>
 			<td>{{$us->empresa}}</td>
 			<td>{{$us->correo}}</td>
+			<td>{{$us->telefono}}</td>
 			<td>{{$us->rol}}</td>
 		<td>
 			<a type="button" href="" class="btn btn-success btn-sm" data-toggle="modal" data-id="{{$us->id}}" data-target="#editarUsuario{{$us->id}}" style="width: 30px" ><i class="fa fa-pencil" aria-hidden="true"></i></a>
