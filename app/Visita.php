0484b90246b67ebfc2d22ddcc68a1ca000ef251a
Carlos cabarcas <carlos.cape.8@gmail.com>
2017-12-08 15:25:22 -0500

    Corregido capacitación

--- app/Visita.php
+++ app/Visita.php
@@ -9,6 +9,6 @@ class Visita extends Model
     protected $table='visitas';
 
     protected $fillable=[
-        'tipo', 'lugar', 'fecha', 'fechainicio', 'fechafin', 'duracion', 'id_empresa', 'motivovisita', 'recoleccion', 'cliente', 'telefono', 'satisfaccion', 'id_consultor', 'id_modulo', 'estado'
+        'tipo', 'lugar', 'fecha', 'fechainicio', 'fechafin', 'duracion', 'id_empresa', 'motivovisita', 'recoleccion', 'cliente', 'telefono', 'satisfaccion', 'id_consultor', 'id_contrato', 'id_modulo', 'estado'
     ];
 }
